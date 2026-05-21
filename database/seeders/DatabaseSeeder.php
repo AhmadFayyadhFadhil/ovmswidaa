a9018379a372d167599d989b573e5ebdc2a60124
Ahmad Fayyadh Fadhil <fadhilahmad1199@gmail.com>
2026-05-21 09:47:03 +0700

    masih eror

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Run role & permission seeder first
+        $this->call(RolePermissionSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Create default Admin user
+        $admin = User::factory()->create([
+            'name'     => 'Administrator',
+            'email'    => 'admin@ovms.test',
+            'password' => Hash::make('password'),
         ]);
+        $admin->assignRole('Admin');
+
+        // Create default GA user
+        $ga = User::factory()->create([
+            'name'     => 'General Affairs',
+            'email'    => 'ga@ovms.test',
+            'password' => Hash::make('password'),
+        ]);
+        $ga->assignRole('GA');
+
+        // Create default Approver user
+        $approver = User::factory()->create([
+            'name'     => 'Manager Approver',
+            'email'    => 'approver@ovms.test',
+            'password' => Hash::make('password'),
+        ]);
+        $approver->assignRole('Approver');
+
+        // Create default Employee user
+        $employee = User::factory()->create([
+            'name'     => 'Employee Test',
+            'email'    => 'employee@ovms.test',
+            'password' => Hash::make('password'),
+        ]);
+        $employee->assignRole('Employee');
+
+        // Create default Driver user
+        $driver = User::factory()->create([
+            'name'     => 'Driver Test',
+            'email'    => 'driver@ovms.test',
+            'password' => Hash::make('password'),
+        ]);
+        $driver->assignRole('Driver');
     }
 }
