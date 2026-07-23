2d2b33d4db6773a2b9b03134b7e26ae92d8d505e
Ahmad Fayyadh Fadhil <fadhilahmad1199@gmail.com>
2026-07-23 13:55:48 +0700

    Trust proxies to enforce https URLs for all resources

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->throttleApi();
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
