727926f79678752b3bad122eced9b761ce52fdb7
Ahmad Fayyadh Fadhil <fadhilahmad1199@gmail.com>
2026-08-03 14:49:28 +0700

    fix(assignment): import RequestStatus enum in AssignmentController

--- app/Http/Controllers/Api/AssignmentController.php
+++ app/Http/Controllers/Api/AssignmentController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Actions\Assignments\AssignDriverAction;
 use App\Actions\Assignments\DriverRespondAction;
+use App\Enums\RequestStatus;
 
 class AssignmentController extends Controller
 {
